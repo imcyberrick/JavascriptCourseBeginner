1fac0cdb99a2aa9fdcd39ee5dcf189675db66278
Riccardo <70761137+riccardobellomi@users.noreply.github.com>
2023-04-05 07:53:34 +0200

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -1,6 +1,11 @@
 let array = [1, 2, 4, 5];
+let array2 = [];
 
 console.log(array);
 console.log(array[2]);
 
 let sum = array[0] + array[2];
+console.log(sum);
+
+array.shift();
+console.log(array);
