1809072b69d71562181750578a54f302ab900965
Riccardo <70761137+riccardobellomi@users.noreply.github.com>
2023-04-10 11:08:42 +0200

    Update objects.js

--- objects.js
+++ objects.js
@@ -8,3 +8,9 @@ console.log(JSON.stringify(jacket));
 
 jacket.size = "L";
 console.log(JSON.stringify(jacket));
+
+jacket["size"] = "S";
+console.log(JSON.stringify(jacket));
+
+delete jacket.size;
+console.log(JSON.stringify(jacket));
